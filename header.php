83a9b597243ac87bc7f72d7cf010eceba486ec50
Bill MacKenty <bmackenty@users.noreply.github.com>
2018-01-10 14:11:02 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,7 @@
+<!-- this file should be named header.php -->
+<nav class="navbar navbar-dark bg-primary">
+
+    <span class="navbar-brand mb-0 h1">Hello World! This is my navbar!
+
+    </span>
+</nav>
