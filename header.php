e3761fbee526d72eae9aff9d681dc274d5c6f1f7
Bill MacKenty <bmackenty@users.noreply.github.com>
2018-01-12 09:57:19 +0100

    added logout link.
    obviously, if the user isn't logged in, we should make this say login....

--- header.php
+++ header.php
@@ -1,7 +1,8 @@
 <!-- this file should be named header.php -->
 <nav class="navbar navbar-dark bg-primary">
 
-    <span class="navbar-brand mb-0 h1">Hello World! This is my navbar!
-
-    </span>
+    <span class="navbar-brand mb-0 h1">Hello World</span>
+    <div class="navbar-nav">
+        <a class="nav-item nav-link" href="logout.php">Logout</a>
+    </div>
 </nav>
