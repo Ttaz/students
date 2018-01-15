9991e557ecdd123e234109608b184f2805b43e89
Bill MacKenty <bmackenty@users.noreply.github.com>
2018-01-15 14:08:21 +0100

    added a comment to add conditional logic here for logged in / logged out users

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 
     <span class="navbar-brand mb-0 h1">Hello World</span>
     <div class="navbar-nav">
+        <!-- some PHP code here to detect if a user is logged in or not -->
         <a class="nav-item nav-link" href="logout.php">Logout</a>
     </div>
 </nav>
