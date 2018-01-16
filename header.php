9d1a6fe0b286d4021f275d21e664862c06e58b5c
Bill MacKenty <bmackenty@users.noreply.github.com>
2018-01-16 15:18:25 +0100

    added some simple logic to detect and respond to a logged in / logged out  user

--- header.php
+++ header.php
@@ -3,7 +3,18 @@
 
     <span class="navbar-brand mb-0 h1">Hello World</span>
     <div class="navbar-nav">
-        <!-- some PHP code here to detect if a user is logged in or not -->
-        <a class="nav-item nav-link" href="logout.php">Logout</a>
+
+        <?php
+        $logged_in = $_SESSION['logged_in'];
+        if ($logged_in) {
+
+            echo '<a class="nav-item nav-link text-white" href="logout.php">Logout</a>';
+
+        } else {
+
+            echo '<a class="nav-item nav-link text-white" href="login.php">Login</a>';
+
+        }
+        
     </div>
 </nav>
